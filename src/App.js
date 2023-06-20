572d1bc4d8ca019766d604c1471c0b8daa5d4822
Cristian Ferreyra <cristianbferreyra@gmail.com>
2023-06-20 19:48:40 -0300

    hasta clase 8 junio completa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Title from "./componenetes/Title";
+import Card from "./componenetes/Card";
 function App() {
   return (
     <div className="App">
       {/* Comentario en JSX */}
       <Title nombre={"Eva!"} />
+      <Card />
       Hola mundo
     </div>
   );
