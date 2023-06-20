f0bc9a2228b980ec469faeacbdbd8ee8eafd6c9c
Cristian Ferreyra <cristianbferreyra@gmail.com>
2023-06-20 12:02:21 -0300

    commit prueba

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import Title from "./componenetes/Title";
 function App() {
-  return <div className="App">Hola mundo</div>;
+  return (
+    <div className="App">
+      {/* Comentario en JSX */}
+      <Title nombre={"Eva!"} />
+      Hola mundo
+    </div>
+  );
 }
 
 export default App;
---
+++ src/componenetes/Title.js
@@ -0,0 +1,6 @@
+import "./Title.css";
+
+export default function Title({ nombre }) {
+  // const nombre = "Eva";
+  return <h1 className="title">Bienvenido {nombre}</h1>;
+}
