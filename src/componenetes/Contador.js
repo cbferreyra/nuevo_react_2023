5fd1e99aeda39b6169c7ec55144f210a0925069a
Cristian Ferreyra <cristianbferreyra@gmail.com>
2023-07-06 13:29:12 -0300

    traigo pokemones y los imprimo en pantalla

--- src/componenetes/Contador.js
+++ src/componenetes/Contador.js
@@ -1,7 +1,29 @@
 export default function Contador() {
+  /* Información general:
+
+    -Cantidad
+    -precio unitario
+    -total
+
+    Información fija:
+    -Precio unitario
+
+    Predeterminados:
+    cantidad: 0
+    Precio_unitario: 500
+    -total: 0
+
+     */
+  const precio_unitario = 500;
+
+  //Primer paso: Setear la cantidad y mostrarla en el párrafo
+  //Memoria del componente
+  //Segundo Paso: Hacer andar el +
+  //Tercer paso: HAcer andar el -
+  //Cuarto paso: Mostrar el total
   return (
     <div>
-      <p>Precio unitario: $XXXXX</p>
+      <p>Precio unitario: $ {precio_unitario}</p>
       <div>
         <button>Restar -</button>
         <p>CANTIDAD</p>
---
+++ src/componenetes/Pokemon.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+export default function Pokemon() {
+  const [loading, setLoading] = useState(true);
+  //Ya sabemos que va a ser un objeto, así que de manera predeterminada usamos {}
+  const [pokemon, setPokemon] = useState({});
+
+  //Los hooks siempre antes del return
+  useEffect(() => {
+    const endpoint = "https://pokeapi.co/api/v2/pokemon/25";
+    fetch(endpoint).then((res) => {
+      console.log(res);
+      if (res.ok === true) {
+        //Todo ok!
+        res.json().then((data) => {
+          //Tenemos el Pokemon!
+          console.log(data);
+          //Guardo al pokemon en la memoria del componente
+          setPokemon(data);
+          //Seteamos a falso el loading
+          setLoading(false);
+        });
+      } else {
+        console.error("Oops,", res.statusText);
+      }
+    });
+  }, []);
+
+  if (loading === true) {
+    return <h1>Cargando Pokemones...</h1>;
+  }
+
+  return (
+    <>
+      <h1>{pokemon.name}</h1>;
+      <p>
+        el peso de {pokemon.name} es {pokemon.weight} kg
+      </p>
+      {pokemon.sprites && <img src={pokemon.sprites.front_default} alt="" />}
+    </>
+  );
+}
